951d64f22a69bab37e0a29b51e0a36a924f83945
idugcoal <idugcoal@gmail.com>
2015-01-22 14:28:16 -0800

    Stack

--- queue.js
+++ queue.js
@@ -15,23 +15,39 @@ function Stack() {
 	}
 }
 
+function Queue() {
+	this.arr = [];
+	this.top = 0;
+	this.enqueue = function enqueue(x) {
+		this.arr.push(x);
+	}
+	this.dequeue = function dequeue() {
+		return this.arr.shift();
+	}
+	this.peek = function peek() {
+		return this.arr[this.top];
+	}
+	this.isEmpty = function isEmpty() {
+		this.length === 0 ? true : false;
+	}
+}
+
 var s = new Stack();
-var t = new Stack();
+var t = new Queue();
 word = prompt("Enter a word: ");
 var isPalindrome = true;
 
 for(var i = 0; i < word.length; i++) {
 	s.push(word[i]);
-	t.push(word[word.length - 1 - i]);
+	t.enqueue(word[i]);
 }
 
 alert("First letter: "+ s.peek());
 
 for(var i = 0; i < word.length && isPalindrome; i++) 
-	if(s.pop() != t.pop())
+	if(s.pop() != t.dequeue())
 		isPalindrome = false;
 
 var result;
 isPalindrome ? result = "Yes, it is a " : result = "No, it is not a ";
-alert(result + "palindrome");
-
+alert(result + "palindrome");	
--- stack2.js
+++ stack2.js
@@ -1,6 +1,6 @@
 var letters = []; // this is our stack
 var rword = "";
-rword = prompt("Enter a word: ");
+putstr("Enter a word: ");
 var word = readline();
 for (var i = 0; i < word.length; i++) {
    rword += letters.pop(); // pop off the stack in reverse order
